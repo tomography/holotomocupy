16de0b254c9a0ea1cb9d53ee24837f2d192ca6e7
Francesco De Carlo <decarlof@gmail.com>
2024-06-18 18:34:05 -0500

    docs build improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -317,10 +317,14 @@ texinfo_documents = [
 # http://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
 
 autodoc_mock_imports = [
+    'astropy', 
     'cupy',
     'dxchange',
     'matplotlib',
+    'numpy',
+    'olefile',
+    'jupyter', 
+    'xraylib', 
     'holotomocupy.cuda_kernels',
-
     ]
 
